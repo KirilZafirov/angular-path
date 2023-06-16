0bfd82f8654b1eefd9cb3599d6235d6efa4a6d33
kzafirov <kiril_zafirov@yahoo.com>
2023-06-16 14:34:31 +0200

    initial commit - empty angular application using the standalone pattern

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [NgSwitch, NgSwitchDefault, NgSwitchCase]
+})
+export class AppComponent {
+  title = 'angular-path';
+}
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+
+
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(BrowserModule)]
+})
+  .catch(err => console.error(err));
